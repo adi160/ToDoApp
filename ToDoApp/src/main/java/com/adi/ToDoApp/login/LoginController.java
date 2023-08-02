714d2919aaabb7510b3d59c908a291b24746e235
adi <adi@192.168.1.25>
2023-08-02 13:59:20 +0530

    III - About logger

--- ToDoApp/src/main/java/com/adi/ToDoApp/login/LoginController.java
+++ ToDoApp/src/main/java/com/adi/ToDoApp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.adi.ToDoApp.login;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+//	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String goToLogin(@RequestParam String name, ModelMap model){
 		model.put("name", name);
+		
+//		logger.info("Request Param is {}", name);
+//		logger.debug("At Debug level");
+//		logger.warn("At Warn level");
 		return "login";
 	}
 
