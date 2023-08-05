01c0069ce5fb236c5ae6e7136834c7bdf777a1ff
adi <adi@192.168.1.25>
2023-08-05 11:27:20 +0530

    VII- Implement update and delete function in app.

--- ToDoApp/src/main/java/com/adi/ToDoApp/todo/TodoController.java
+++ ToDoApp/src/main/java/com/adi/ToDoApp/todo/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 
@@ -46,7 +47,32 @@ public class TodoController {
 			return "todo";
 		}
 		String username = (String)model.get("name");
-		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+		todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+		return "redirect:list-todos";
+	}
+	
+	@RequestMapping("delete-todo")
+	public String deleteTodo(@RequestParam int id) {
+		todoService.deleteById(id);
+		return "redirect:list-todos";
+	}
+	
+	@RequestMapping(value ="update-todo", method=RequestMethod.GET)
+	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+		Todo todo = todoService.findById(id);
+		model.addAttribute("todo", todo);
+		return "todo";
+	}
+	
+	@RequestMapping(value ="update-todo", method=RequestMethod.POST)
+	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		String username = (String)model.get("name");
+		todo.setUsername(username);
+		todoService.updateTodo(todo);
 		return "redirect:list-todos";
 	}
 
--- ToDoApp/src/main/java/com/adi/ToDoApp/todo/TodoService.java
+++ ToDoApp/src/main/java/com/adi/ToDoApp/todo/TodoService.java
@@ -3,8 +3,11 @@ package com.adi.ToDoApp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
 
 @Service
 public class TodoService {
@@ -14,7 +17,7 @@ public class TodoService {
 	private static int todosCount = 0;
 	
 	static {
-		todos.add(new Todo(++todosCount, "Aditya", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Aditya", "Learn AWS certified", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(++todosCount, "Aditya", "Learn DevOps", LocalDate.now().plusYears(2), false));
 		todos.add(new Todo(++todosCount, "Aditya", "Learn Azure", LocalDate.now().plusYears(3), false));
 	}
@@ -25,6 +28,22 @@ public class TodoService {
 	
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done ) {
 		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
+	}
+	
+	public void deleteById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
 		todos.add(todo);
 	}
 
